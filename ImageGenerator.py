978ef3b32f18ed76808db9a637b3191e344017c5
silvermoood <serebryakova.olga.04@mail.ru>
2022-12-18 00:18:58 +0300

    Изменен размер графиков и ускорено их создание

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -3,41 +3,40 @@ from LogicFunction import *
 
 # Создаем белый квадрат
 def graph(func: LogicFunction):
-    f=LogicFunction('a+b+c')
-    img = Image.new('RGBA', (len(func.get_variables())*100,len(func.get_variables())*100+100), 'white')
+    img = Image.new('RGBA', (2 ** len(func.get_variables())*100,len(func.get_variables())*100+100), 'white')
     draw = ImageDraw.Draw(img)
+    gbt=func.generate_boolean_table()
     # Рисуем результат функции
     draw_text = ImageDraw.Draw(img)
-    draw_text.text((3, 12),'func',fill=('black'))
-    for j in range(len(f.generate_boolean_table())):
-        string = f.generate_boolean_table()[j]
+    draw_text.text((3, 20),'func',fill=('black'))
+    for j in range(len(gbt)):
+        string = gbt[j]
         if int(string[1]) == 0:
-            draw.line((10 + 20 * (j + 1), 20, 10 + 20 * (j + 1) + 20, 20), fill='blue', width=3)
+            draw.line((10 + 45 * (j + 1), 50, 10 + 45 * (j + 1) + 45, 50), fill='blue', width=4)
         else:
-            draw.line((10 + 20 * (j + 1), 10, 10 + 20 * (j + 1) + 20, 10), fill='blue', width=3)
-        for j in range(1, len(f.generate_boolean_table())):
-            if f.generate_boolean_table()[j - 1][1] != f.generate_boolean_table()[j][1]:
-                draw.line((10 + 20 * (j + 1), 10, 10 + 20 * (j + 1), 20), fill='blue', width=3)
+            draw.line((10 + 45 * (j + 1), 20, 10 + 45 * (j + 1) + 45, 20), fill='blue', width=4)
+        for j in range(1, len(gbt)):
+            if gbt[j - 1][1] != gbt[j][1]:
+                draw.line((10 + 45 * (j + 1), 20, 10 + 45 * (j + 1), 50), fill='blue', width=4)
     # Рисуем значения переменных
-    for i in range(len(f.get_variables())):
-        name=f.get_variables()[i]
+    for i in range(len(func.get_variables())):
+        name=func.get_variables()[i]
         draw_text = ImageDraw.Draw(img)
-        draw_text.text((10, i * 20 + 42 ), name, fill=('black'))
+        draw_text.text((10, i * 50 + 90), name, fill=('black'))
 
-    for i in range(len(f.get_variables())):
-        for j in range(len(f.generate_boolean_table())):
-            string=f.generate_boolean_table()[j]
-            line = i * 20 + 10
+    for i in range(len(func.get_variables())):
+        for j in range(len(gbt)):
+            string=gbt[j]
+            line = i * 50 + 10
             if int(string[0][i]) == 0:
-                draw.line((10 + 20*(j+1), line + 40, 10 + 20*(j+1) + 20, line + 40), fill='blue', width=3)
+                draw.line((10 + 45*(j+1), line + 90, 10 + 45*(j+1) + 45, line + 90), fill='blue', width=4)
             else:
-                draw.line((10 + 20*(j+1), line + 30, 10 + 20*(j+1) + 20, line+ 30), fill='blue', width=3)
-        for j in range(1,len(f.generate_boolean_table())):
-            line = i * 20 + 10
-            if f.generate_boolean_table()[j-1][0][i] != f.generate_boolean_table()[j][0][i]:
-                draw.line((10 + 20*(j+1), line + 30, 10 + 20*(j+1), line + 40), fill='blue', width=3)
-
+                draw.line((10 + 45*(j+1), line + 70, 10 + 45*(j+1) + 45, line + 70), fill='blue', width=4)
+        for j in range(1,len(gbt)):
+            line = i * 50 + 10
+            if gbt[j-1][0][i] != gbt[j][0][i]:
+                draw.line((10 + 45*(j+1), line + 70, 10 + 45*(j+1), line + 90), fill='blue', width=4)
 
     img.show()
 
-graph(LogicFunction('a+b+c'))
+graph(LogicFunction('!a+b'))
